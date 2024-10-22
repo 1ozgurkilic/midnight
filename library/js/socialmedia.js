8c2e83f9f286ff127ad2b865ce586bfb142b4b9c
Özgür <141361249+1ozgurkilic@users.noreply.github.com>
2024-10-22 12:09:34 +0300

    Delete library directory

--- library/js/socialmedia.js
+++
@@ -1,6 +0,0 @@
-const backButton = document.getElementById('backButton');
-
-backButton.addEventListener('click', function (event) {
-    event.preventDefault(); // Varsayılan davranışı engelle
-    window.history.back(); // Önceki sayfaya git
-});
